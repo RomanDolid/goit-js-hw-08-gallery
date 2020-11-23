68289805d297203dac649d9cedda8b47b90f2233
RomanDolid <dolid.rv@gmail.com>
2020-11-23 20:04:38 +0200

    домашка

---
+++ js/main.js
@@ -0,0 +1,70 @@
+import galleryList from "./gallery-items.js";
+
+const galleryRef = document.querySelector(".js-gallery");
+const openModalRef = document.querySelector(".js-lightbox");
+const imgModalRef = document.querySelector(".lightbox__image");
+const closeModalRef = document.querySelector('[data-action="close-lightbox"]');
+const closeOverlayRef = document.querySelector(".lightbox__overlay");
+
+const galleryItems = createGallery(galleryList);
+galleryRef.insertAdjacentHTML("afterbegin", galleryItems);
+
+galleryRef.addEventListener("click", galleryClick);
+closeModalRef.addEventListener("click", () => {
+  closeModal();
+});
+closeOverlayRef.addEventListener("click", () => {
+  closeModal();
+});
+window.addEventListener("keydown", (event) => {
+  if (event.code !== "Escape") {
+    return;
+  }
+  closeModal();
+});
+
+function createGallery(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a
+        class="gallery__link"
+        href="${original}"
+        >
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </li>`;
+    })
+    .join("");
+}
+
+function galleryClick(event) {
+  event.preventDefault();
+  const imgRef = event.target;
+
+  if (imgRef.nodeName !== "IMG") {
+    return;
+  }
+
+  const originalImageURL = imgRef.dataset.source;
+  setOriginalImageSrc(originalImageURL);
+  openModal();
+}
+
+function setOriginalImageSrc(url) {
+  imgModalRef.src = url;
+}
+
+function openModal() {
+  openModalRef.classList.add("is-open");
+}
+
+function closeModal() {
+  openModalRef.classList.remove("is-open");
+  imgModalRef.src = "";
+}
